ac1eeb0a6da73b32c0fb8c1d721a39f6cd96bb9b
Devankit Sahu <dev738288@gmail.com>
2024-01-22 14:00:19 +0530

    comment some code

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ export const newUser = catchAsyncError(async (req, res, next) => {
     .cookie("jwtToken", token, {
       expires: new Date(Date.now() + 1800000),
       httpOnly: true,
-      secure: true,
+      // secure: true,
     })
     .status(200)
     .json({
