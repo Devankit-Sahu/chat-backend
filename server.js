820285f9084761a9d2717621ce43aa442a664ccd
Devankit Sahu <dev738288@gmail.com>
2024-01-28 16:01:40 +0530

    add frontend url

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ await connectDatabase()
     });
     const io = new Server(server, {
       cors: {
-        origin: "http://localhost:5173",
+        origin: "https://chat-app-alpha-seven-93.vercel.app",
       },
     });
 
